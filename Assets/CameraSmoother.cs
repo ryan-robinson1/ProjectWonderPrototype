1fcf3d8a7c43fe595352e72b8726cfb0a92fa90a
ryan-robinson1 <69409000+ryan-robinson1@users.noreply.github.com>
2020-09-16 23:51:56 -0400

    Attempt at Camera Smoothing

---
+++ Assets/CameraSmoother.cs
@@ -0,0 +1,24 @@
+﻿using ExitGames.Client.Photon;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSmoother : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float smoothTime = 1f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, 0));
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+    }
+}
